b6a67735f7c6da05e27877733c4977e08c68be82
Abraham Duno <71507184+3dunoabraham@users.noreply.github.com>
2023-01-13 21:54:28 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,10 @@
-import '../styles/globals.css'
-import '../styles/spirit.css'
-import '../styles/theme.css'
-
 import type { AppProps } from 'next/app'
 
+
+import '../styles/globals.css'
+import '../styles/css.css'
+import '../styles/theme.css'
+import '../styles/duno.css'
 function MyApp({ Component, pageProps }: AppProps) {
   	return <div className="dark-theme">
   		<Component  {...pageProps} />
