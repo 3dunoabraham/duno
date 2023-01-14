2b249ceb10017d7080ae3333c6e61618ec863474
Abraham Duno <71507184+3dunoabraham@users.noreply.github.com>
2023-01-13 22:48:29 -0400

    Update and rename tokens.js to contact.js

---
+++ pages/contact.js
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+
+export default () => {
+    return (
+    <div className="">
+        <div className="tx-xl">
+            <Link href="/">
+                <a>volver a home</a>
+            </Link>
+        </div>
+        <h1>page</h1>
+        <h1>page2</h1>
+    </div>
+    )
+}
